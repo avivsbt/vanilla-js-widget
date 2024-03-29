ceaaf99d5c6b5b3c259592faf4c7479018a09eac
avivsbt <avivsbt@gmail.com>
2024-03-29 18:21:59 +0300

    [Api] refactoring

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import { store } from '../store/index.js'
 import { data } from '../lib/data.js'
 import { fetchRequest } from '../services/api.js';
 
-export function getRecommendations(
+export async function getSponsoredRecommendations(
   publisher_id = 'taboola-templates',
   app_type = 'desktop',
   app_apikey = 'f9040ab1b9c802857aa783c469d0e0ff7e7366e4',
@@ -10,17 +10,21 @@ export function getRecommendations(
 ) {
   const url = `http://api.taboola.com/1.0/json/${publisher_id}/recommendations.get?app.type=${app_type}&app.apikey=${app_apikey}&count=100&source.type=video&source.id=${source_id}`;
 
-  fetchRequest(url).then(response => {
-
+  try {
+    const response = await fetchRequest(url);
+    
     const mappedData = {};
 
     data.forEach(item => {
       item.categories.forEach(category => {
-        item = {...item, isViewed: false}
         mappedData[category] ? mappedData[category].push(item) : mappedData[category] = [item];
       });
     });
 
-    store.dispatch("updateSponsoredRecommendations", [mappedData]);
-  });
+    store.dispatch("setSponsoredRecommendations", [mappedData]);
+    
+  } catch (error) {
+    throw error
+  }
+
 }
