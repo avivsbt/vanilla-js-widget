3cf471e8e0e87c2876b3e1c32907c8a822407072
avivsbt <avivsbt@gmail.com>
2024-03-31 10:01:15 +0300

    [IE] fixes

--- ie/script.js
+++ ie/script.js
@@ -1,63 +1,62 @@
-
 var data = [
   {
-      "type": "video",
-      "thumbnail": [
-          {
-              "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fmedia.meltyfashion.fr%2Farticle-2042273-ratio265_640-f532084%2Ffashion-week-imprime-automne-hiver-maison.jpg"
-          }
-      ],
-      "description": "Apprenez à éviter les fautes de goût pendant votre shopping mode avec le guide shopping",
-      "name": "Les imprimés tendance de 2014",
-      "created": "Wed, 02 Apr 2014 00:00:00 UTC",
-      "branding": "Elle",
-      "duration": "0",
-      "views": "0",
-      "categories": [
-          "fr"
-      ],
-      "id": "~~V1~~4166874741841185432~~ZZjwieuFi0udh1RZS1JXsHgEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePejIoJl3WKIIrm4Yg_30fVE1XNvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ17hW18gOeBKWEuq6da9CEkY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe",
-      "origin": "sponsored",
-      "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__f49e2adc26849a89a786a49d25ca6dea__1b248dfe29619f0d68dbb87312668aaf&response.session=v2_71c9b47dc345508c25861d5803b29741_83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed_1711865453_1711865453_CNawjgYQswsYormHmekxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E4166874741841185432%7E%7EZZjwieuFi0udh1RZS1JXsHgEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePejIoJl3WKIIrm4Yg_30fVE1XNvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ17hW18gOeBKWEuq6da9CEkY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe&item.type=video&sig=f8c66250ba14d17606145a386009aadac7f092898d65&redir=http%3A%2F%2Fwww.elle.fr%2FMode%2FLe-guide-shopping%2FPrintemps-Ete-2014%2FLes-50-imprimes-de-la-saison%3Futm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCj1jf7b_9Tcrzs%23tblciGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCj1jf7b_9Tcrzs&ui=83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMjM1OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwjQNxD-ShgwZGMI1xYQ1R8YI2RjCO3__________wEQ7f__________ARgTZGMI0gMQ4AYYCGRjCJYUEJgcGBhkYwjvAxCJBxgJZGMIxV0Q3HoYPmRjCPQUEJ4dGB9kYwikJxCKNRgvZHgBgAGsTYgBybuz6gGQAReYAaK5h5npMdsBEAHcAQ"
+    "type": "video",
+    "thumbnail": [
+      {
+        "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fmedia.meltyfashion.fr%2Farticle-2042273-ratio265_640-f532084%2Ffashion-week-imprime-automne-hiver-maison.jpg"
+      }
+    ],
+    "description": "Apprenez à éviter les fautes de goût pendant votre shopping mode avec le guide shopping",
+    "name": "Les imprimés tendance de 2014",
+    "created": "Wed, 02 Apr 2014 00:00:00 UTC",
+    "branding": "Elle",
+    "duration": "0",
+    "views": "0",
+    "categories": [
+      "fr"
+    ],
+    "id": "~~V1~~4166874741841185432~~ZZjwieuFi0udh1RZS1JXsHgEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePejIoJl3WKIIrm4Yg_30fVE1XNvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ17hW18gOeBKWEuq6da9CEkY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe",
+    "origin": "sponsored",
+    "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__f49e2adc26849a89a786a49d25ca6dea__1b248dfe29619f0d68dbb87312668aaf&response.session=v2_71c9b47dc345508c25861d5803b29741_83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed_1711865453_1711865453_CNawjgYQswsYormHmekxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E4166874741841185432%7E%7EZZjwieuFi0udh1RZS1JXsHgEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePejIoJl3WKIIrm4Yg_30fVE1XNvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ17hW18gOeBKWEuq6da9CEkY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe&item.type=video&sig=f8c66250ba14d17606145a386009aadac7f092898d65&redir=http%3A%2F%2Fwww.elle.fr%2FMode%2FLe-guide-shopping%2FPrintemps-Ete-2014%2FLes-50-imprimes-de-la-saison%3Futm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCj1jf7b_9Tcrzs%23tblciGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCj1jf7b_9Tcrzs&ui=83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMjM1OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwjQNxD-ShgwZGMI1xYQ1R8YI2RjCO3__________wEQ7f__________ARgTZGMI0gMQ4AYYCGRjCJYUEJgcGBhkYwjvAxCJBxgJZGMIxV0Q3HoYPmRjCPQUEJ4dGB9kYwikJxCKNRgvZHgBgAGsTYgBybuz6gGQAReYAaK5h5npMdsBEAHcAQ"
   },
   {
-      "type": "video",
-      "thumbnail": [
-          {
-              "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fespndeportes-assets.espn.go.com%2F2003%2Fphotos2014%2F0122%2Fg_Lifesteja_2x3.jpg"
-          }
-      ],
-      "description": "Ganaron Cibulkova y Li y avanzaron a la final del Abierto de Australia",
-      "name": "Ganaron Cibulkova y Li y avanzaron a la final del Abierto de Australia - Tenis",
-      "created": "Wed, 22 Jan 2014 23:22:29 UTC",
-      "branding": "ESPN deportes",
-      "duration": "0",
-      "views": "0",
-      "categories": [
-          "en"
-      ],
-      "id": "~~V1~~4194129851886853795~~ixOeaP1RSqw3gPCiqVniUHjkpWMAeolRpeSAL57DQcraxJUMYN6PI0G8UwQ4xoq4BNTe-Zc-jkR3zbXih2M1Tu7QA-g8rM2w-kqFxRHx69rg_NZDmGr6sm9BYE-mD177GXLwsS3fkWtpBPA0sLI4oo6zhr_uog6GC70OqLAd6RE",
-      "origin": "sponsored",
-      "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__f49e2adc26849a89a786a49d25ca6dea__1b248dfe29619f0d68dbb87312668aaf&response.session=v2_71c9b47dc345508c25861d5803b29741_83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed_1711865453_1711865453_CNawjgYQswsYormHmekxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E4194129851886853795%7E%7EixOeaP1RSqw3gPCiqVniUHjkpWMAeolRpeSAL57DQcraxJUMYN6PI0G8UwQ4xoq4BNTe-Zc-jkR3zbXih2M1Tu7QA-g8rM2w-kqFxRHx69rg_NZDmGr6sm9BYE-mD177GXLwsS3fkWtpBPA0sLI4oo6zhr_uog6GC70OqLAd6RE&item.type=video&sig=8ca66e279b817ea1c6ccb0523b275e09977d99e10a1b&redir=http%3A%2F%2Fespndeportes.espn.go.com%2Fnews%2Fstory%3Fid%3D2005759%26s%3Dten%26type%3Dstory%26utm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCjksd7k_8j3wrUB%23tblciGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCjksd7k_8j3wrUB&ui=83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMjM1OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwjQNxD-ShgwZGMI1xYQ1R8YI2RjCO3__________wEQ7f__________ARgTZGMI0gMQ4AYYCGRjCJYUEJgcGBhkYwjvAxCJBxgJZGMIxV0Q3HoYPmRjCPQUEJ4dGB9kYwikJxCKNRgvZHgBgAGsTYgBybuz6gGQAReYAaK5h5npMdsBEAHcAQ"
+    "type": "video",
+    "thumbnail": [
+      {
+        "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fespndeportes-assets.espn.go.com%2F2003%2Fphotos2014%2F0122%2Fg_Lifesteja_2x3.jpg"
+      }
+    ],
+    "description": "Ganaron Cibulkova y Li y avanzaron a la final del Abierto de Australia",
+    "name": "Ganaron Cibulkova y Li y avanzaron a la final del Abierto de Australia - Tenis",
+    "created": "Wed, 22 Jan 2014 23:22:29 UTC",
+    "branding": "ESPN deportes",
+    "duration": "0",
+    "views": "0",
+    "categories": [
+      "en"
+    ],
+    "id": "~~V1~~4194129851886853795~~ixOeaP1RSqw3gPCiqVniUHjkpWMAeolRpeSAL57DQcraxJUMYN6PI0G8UwQ4xoq4BNTe-Zc-jkR3zbXih2M1Tu7QA-g8rM2w-kqFxRHx69rg_NZDmGr6sm9BYE-mD177GXLwsS3fkWtpBPA0sLI4oo6zhr_uog6GC70OqLAd6RE",
+    "origin": "sponsored",
+    "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__f49e2adc26849a89a786a49d25ca6dea__1b248dfe29619f0d68dbb87312668aaf&response.session=v2_71c9b47dc345508c25861d5803b29741_83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed_1711865453_1711865453_CNawjgYQswsYormHmekxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E4194129851886853795%7E%7EixOeaP1RSqw3gPCiqVniUHjkpWMAeolRpeSAL57DQcraxJUMYN6PI0G8UwQ4xoq4BNTe-Zc-jkR3zbXih2M1Tu7QA-g8rM2w-kqFxRHx69rg_NZDmGr6sm9BYE-mD177GXLwsS3fkWtpBPA0sLI4oo6zhr_uog6GC70OqLAd6RE&item.type=video&sig=8ca66e279b817ea1c6ccb0523b275e09977d99e10a1b&redir=http%3A%2F%2Fespndeportes.espn.go.com%2Fnews%2Fstory%3Fid%3D2005759%26s%3Dten%26type%3Dstory%26utm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCjksd7k_8j3wrUB%23tblciGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCjksd7k_8j3wrUB&ui=83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMjM1OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwjQNxD-ShgwZGMI1xYQ1R8YI2RjCO3__________wEQ7f__________ARgTZGMI0gMQ4AYYCGRjCJYUEJgcGBhkYwjvAxCJBxgJZGMIxV0Q3HoYPmRjCPQUEJ4dGB9kYwikJxCKNRgvZHgBgAGsTYgBybuz6gGQAReYAaK5h5npMdsBEAHcAQ"
   },
   {
-      "type": "video",
-      "thumbnail": [
-          {
-              "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fcdn.taboolasyndication.com%2Fsv%2Fimages%2Fautoservice124.png"
-          }
-      ],
-      "name": "Amazing U.F.O. Home in Puerto Rico",
-      "created": "Wed, 08 Aug 2012 20:06:17 UTC",
-      "branding": "The New York Times",
-      "duration": "106",
-      "views": "0",
-      "categories": [
-          "en"
-      ],
-      "id": "~~V1~~6596092009586762943~~Yy8iEO2YJatBqPpJiQzaWcjX3ZbCIHkjiiGd10MMRHn47DqYi40_M7SmxblRZ1jwDaz3Yz2A7XHhwrO5mF5aNXK9LZf4s-wN0e-IrD14BQ5zRT2aBImfuuldQsAIqg2VA8znGKQO1bJpPNJmvJW5bbqtfNB9PSjDBlig62vbwfc",
-      "origin": "sponsored",
-      "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__f49e2adc26849a89a786a49d25ca6dea__1b248dfe29619f0d68dbb87312668aaf&response.session=v2_71c9b47dc345508c25861d5803b29741_83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed_1711865453_1711865453_CNawjgYQswsYormHmekxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E6596092009586762943%7E%7EYy8iEO2YJatBqPpJiQzaWcjX3ZbCIHkjiiGd10MMRHn47DqYi40_M7SmxblRZ1jwDaz3Yz2A7XHhwrO5mF5aNXK9LZf4s-wN0e-IrD14BQ5zRT2aBImfuuldQsAIqg2VA8znGKQO1bJpPNJmvJW5bbqtfNB9PSjDBlig62vbwfc&item.type=video&sig=0c7f92049be12e809a478204414d6d69dc4ee2070275&redir=http%3A%2F%2Fvideo.nytimes.com%2Fvideo%2F2012%2F08%2F08%2Fgarden%2F100000001705279%2Fufo-home.html%3Futm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCiYjt2c0rS1wTs%23tblciGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCiYjt2c0rS1wTs&ui=83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMjM1OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwjQNxD-ShgwZGMI1xYQ1R8YI2RjCO3__________wEQ7f__________ARgTZGMI0gMQ4AYYCGRjCJYUEJgcGBhkYwjvAxCJBxgJZGMIxV0Q3HoYPmRjCPQUEJ4dGB9kYwikJxCKNRgvZHgBgAGsTYgBybuz6gGQAReYAaK5h5npMdsBEAHcAQ"
+    "type": "video",
+    "thumbnail": [
+      {
+        "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fcdn.taboolasyndication.com%2Fsv%2Fimages%2Fautoservice124.png"
+      }
+    ],
+    "name": "Amazing U.F.O. Home in Puerto Rico",
+    "created": "Wed, 08 Aug 2012 20:06:17 UTC",
+    "branding": "The New York Times",
+    "duration": "106",
+    "views": "0",
+    "categories": [
+      "en"
+    ],
+    "id": "~~V1~~6596092009586762943~~Yy8iEO2YJatBqPpJiQzaWcjX3ZbCIHkjiiGd10MMRHn47DqYi40_M7SmxblRZ1jwDaz3Yz2A7XHhwrO5mF5aNXK9LZf4s-wN0e-IrD14BQ5zRT2aBImfuuldQsAIqg2VA8znGKQO1bJpPNJmvJW5bbqtfNB9PSjDBlig62vbwfc",
+    "origin": "sponsored",
+    "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__f49e2adc26849a89a786a49d25ca6dea__1b248dfe29619f0d68dbb87312668aaf&response.session=v2_71c9b47dc345508c25861d5803b29741_83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed_1711865453_1711865453_CNawjgYQswsYormHmekxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E6596092009586762943%7E%7EYy8iEO2YJatBqPpJiQzaWcjX3ZbCIHkjiiGd10MMRHn47DqYi40_M7SmxblRZ1jwDaz3Yz2A7XHhwrO5mF5aNXK9LZf4s-wN0e-IrD14BQ5zRT2aBImfuuldQsAIqg2VA8znGKQO1bJpPNJmvJW5bbqtfNB9PSjDBlig62vbwfc&item.type=video&sig=0c7f92049be12e809a478204414d6d69dc4ee2070275&redir=http%3A%2F%2Fvideo.nytimes.com%2Fvideo%2F2012%2F08%2F08%2Fgarden%2F100000001705279%2Fufo-home.html%3Futm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCiYjt2c0rS1wTs%23tblciGiBBg1jDVki3_0EvGz02T3GXatlY5OBYG-VIbtTdn0hGhCC5BCiYjt2c0rS1wTs&ui=83a6d5a9-769b-4de1-8a24-a648d78779a8-tuctd0283ed&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMjM1OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwjQNxD-ShgwZGMI1xYQ1R8YI2RjCO3__________wEQ7f__________ARgTZGMI0gMQ4AYYCGRjCJYUEJgcGBhkYwjvAxCJBxgJZGMIxV0Q3HoYPmRjCPQUEJ4dGB9kYwikJxCKNRgvZHgBgAGsTYgBybuz6gGQAReYAaK5h5npMdsBEAHcAQ"
   }
 ];
 var sponsoredRecommendations = null;
@@ -70,9 +69,13 @@ function getSponsoredRecommendations() {
     console.log('httpRequest', response);
     mappedData = {};
 
-    data.forEach(item => {
-      item.categories.forEach(category => {
-        mappedData[category] ? mappedData[category].push(item) : mappedData[category] = [item];
+    data.forEach(function (item) {
+      item.categories.forEach(function (category) {
+        if (mappedData[category]) {
+          mappedData[category].push(item);
+        } else {
+          mappedData[category] = [item];
+        }
       });
     });
     sponsoredRecommendations = mappedData;
@@ -80,7 +83,11 @@ function getSponsoredRecommendations() {
   });
 }
 
-function httpRequest(url, callback, options = { headers: {}, body: {} }) {
+function httpRequest(url, callback, options) {
+
+  if (options === undefined) {
+    options = { headers: {}, body: {} };
+  }
 
   var xhr = new XMLHttpRequest();
 
@@ -113,7 +120,8 @@ function httpRequest(url, callback, options = { headers: {}, body: {} }) {
 }
 
 function generateBanner() {
-  for (const element of sponsoredsElements) {
+  for (var i = 0; i < sponsoredsElements.length; i++) {
+    var element = sponsoredsElements[i];
 
     var attrCategory = element.getAttribute('category');
     var attrComponent = element.getAttribute('component');
@@ -130,7 +138,6 @@ function generateBanner() {
       var component = bannerSliderImgComponent(attrCategory, attrAmount);
       element.appendChild(component);
     }
-
   }
 }
 
@@ -143,26 +150,26 @@ function bannerImgComponent(data) {
     '   <a target="_blank" class="link">' +
     '       <div class="wrapper-img"><img class="img"/></div>' +
     '       <h1 class="title"></h1>' +
-    '   <a>' +
+    '   </a>' +
     '   <div class="type">' +
     '       <span class="branding"></span> | ' +
     '       <span class="origin"></span>' +
     '   </div>' +
     '</div>';
 
-  var link = template.content.querySelector('.link');
-  var img = template.content.querySelector('.img');
-  var title = template.content.querySelector('.title');
-  var origin = template.content.querySelector('.origin');
-  var branding = template.content.querySelector('.branding');
+  var link = template.querySelector('.link');
+  var img = template.querySelector('.img');
+  var title = template.querySelector('.title');
+  var origin = template.querySelector('.origin');
+  var branding = template.querySelector('.branding');
 
   title.textContent = data.name;
-  img.setAttribute('src', data.thumbnail[0].url);
-  link.setAttribute('href', data.url);
+  img.src = data.thumbnail[0].url;
+  link.href = data.url;
   origin.textContent = data.origin;
   branding.textContent = data.branding;
 
-  container.appendChild(template.content);
+  container.appendChild(template);
 
   return container;
 }
@@ -176,16 +183,16 @@ function bannerSliderImgComponent(category, amount) {
     '   <div class="slider"></div>' +
     '</div>';
 
-  var slider = template.content.querySelector('.slider');
+  var slider = template.querySelector('.slider');
 
-  for (let i = 0; i < amount; i++) {
+  for (var i = 0; i < amount; i++) {
     if (sponsoredRecommendations[category].length === 0) break;
-    let component = bannerImgComponent(sponsoredRecommendations[category][0]);
+    var component = bannerImgComponent(sponsoredRecommendations[category][0]);
     sponsoredRecommendations[category].shift();
     slider.appendChild(component);
   }
 
-  container.appendChild(template.content);
+  container.appendChild(template);
 
   return container;
 }
