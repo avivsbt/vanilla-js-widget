88b369d892e1e575accf2d0454b827ccb640d20d
avivsbt <avivsbt@gmail.com>
2024-03-31 08:10:43 +0300

    [IE] refactoring

--- ie/script.js
+++ ie/script.js
@@ -1,20 +1,102 @@
 
+var sponsoreds = [
+  {
+    "type": "video",
+    "thumbnail": [
+      {
+        "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/https%3A%2F%2Fimg-s-msn-com.akamaized.net%2Ftenant%2Famp%2Fentityid%2FBBREEpQ.img%3Fh%3D217%26amp%3Bw%3D300%26amp%3Bm%3D6%26amp%3Bq%3D60%26amp%3Bo%3Df%26amp%3Bl%3Df",
+        "width": "301",
+        "height": "217"
+      }
+    ],
+    "description": "New York - Americký dolar dnes mírně oslabuje, od začátku letošního roku si však vůči koši předních měn připisuje zhruba 4,5 procenta. To podle agentury Reuters představuje jeho nejlepší celoroční výkon za tři roky.",
+    "name": "Dolar míří k nejlepšímu celoročnímu výkonu za tři roky",
+    "created": "Mon, 11 Feb 2019 08:42:35 UTC",
+    "branding": "msn.cs-cz",
+    "duration": "0",
+    "views": "0",
+    "categories": [
+      "en"
+    ],
+    "id": "~~V1~~-4505373341331687391~~3WCIXV0rO8qfEzUzxptdbuSfaRsf2w_YUiFgCby9qAnbT-8E6exUyK7uNwYXAaNVKLl88y_nkRLcozD0jNBd7V7_9yYhXzQRArYd9yEEmIQv-g3BPZqv3IrMOsHf8vWe",
+    "origin": "sponsored",
+    "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__98990284f486efafa2557dea6d2b2e2b__7a48870ae0318c93bf89a2be46b1e6ec&response.session=v2_a6a5ab31ae87750c87c46faf01c95b38_bb1dd47d-60fc-4770-8aae-558fde6b18bd-tuctd00aba3_1711744547_1711744547_CNawjgYQswsYqvaz3-gxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E-4505373341331687391%7E%7E3WCIXV0rO8qfEzUzxptdbuSfaRsf2w_YUiFgCby9qAnbT-8E6exUyK7uNwYXAaNVKLl88y_nkRLcozD0jNBd7V7_9yYhXzQRArYd9yEEmIQv-g3BPZqv3IrMOsHf8vWe&item.type=video&sig=0fab158bcd8f4e63deadd710485b99f627dd9a2aced3&redir=https%3A%2F%2Fwww.msn.com%2Fcs-cz%2Fzpravy%2Fother%2Fdolar-m%C3%AD%C5%99%C3%AD-k-nejlep%C5%A1%C3%ADmu-celoro%C4%8Dn%C3%ADmu-v%C3%BDkonu-za-t%C5%99i-roky%2Far-BBREJdo%3Futm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiD9r8gQ15Vcstvn4Lp2FpJ_7JqJra4_4SIYpogG_z_zYCC5BCjP6cuC8a-l9Qs%23tblciGiD9r8gQ15Vcstvn4Lp2FpJ_7JqJra4_4SIYpogG_z_zYCC5BCjP6cuC8a-l9Qs&ui=bb1dd47d-60fc-4770-8aae-558fde6b18bd-tuctd00aba3&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMTI3OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwiiNRDiRhgyZGMI1xYQ1R8YI2RjCJ5BEJVYGDZkYwjSAxDgBhgIZGMIlhQQmBwYGGRjCPQUEJ4dGB9kYwikJxCDNRgvZHgBgAGsTYgBybuz6gGQAReYAar2s9_oMdsBEAHcAQ"
+  },
+  {
+    "type": "video",
+    "thumbnail": [
+      {
+        "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fcdn-elle.ladmedia.fr%2Fvar%2Fplain_site%2Fstorage%2Fimages%2Felle-a-table%2Fles-dossiers-de-la-redaction%2Fnews-de-la-redaction%2Fyannick-alleno-et-mathieu-viannay-leur-diner-a-quatre-mains-2687490%2F46718994-1-fre-FR%2FYannick-Alleno-et-Mathieu-Viannay-leur-diner-a-quatre-mains_reference.jpg"
+      }
+    ],
+    "description": "C’est dimanche que s’ouvriront les portes du World Omnivore Tour, le plus grand festival de cuisine en France. L’occasion pour les plus grands chefs de la planète de se retrouver et de rivaliser de créativité et d‘ingéniosité autour de performances culino-gastronomiques. On est allé à...",
+    "name": "Yannick Alléno et Mathieu Viannay : leur dîner à quatre mains ! - Elle à Table",
+    "created": "Mon, 10 Mar 2014 00:00:00 UTC",
+    "branding": "Cuisine",
+    "duration": "0",
+    "views": "0",
+    "categories": [
+      "en"
+    ],
+    "id": "~~V1~~1204410231236800330~~5khWG3r4qdI2H7O_KccoPngEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePejFjdyaAfJpxf93xCM8jBXM3NvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ16geeKmBzsC8tOXDDFySaFY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe",
+    "origin": "sponsored",
+    "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__98990284f486efafa2557dea6d2b2e2b__7a48870ae0318c93bf89a2be46b1e6ec&response.session=v2_a6a5ab31ae87750c87c46faf01c95b38_bb1dd47d-60fc-4770-8aae-558fde6b18bd-tuctd00aba3_1711744547_1711744547_CNawjgYQswsYqvaz3-gxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E1204410231236800330%7E%7E5khWG3r4qdI2H7O_KccoPngEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePejFjdyaAfJpxf93xCM8jBXM3NvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ16geeKmBzsC8tOXDDFySaFY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe&item.type=video&sig=3fd798363b422aa834a85e4066b6dda44bcd01e1e4c6&redir=http%3A%2F%2Fwww.elle.fr%2FElle-a-Table%2FLes-dossiers-de-la-redaction%2FNews-de-la-redaction%2FYannick-Alleno-et-Mathieu-Viannay-leur-diner-a-quatre-mains-2687490%3Futm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiD9r8gQ15Vcstvn4Lp2FpJ_7JqJra4_4SIYpogG_z_zYCC5BCjunvz3sOKD1t4B%23tblciGiD9r8gQ15Vcstvn4Lp2FpJ_7JqJra4_4SIYpogG_z_zYCC5BCjunvz3sOKD1t4B&ui=bb1dd47d-60fc-4770-8aae-558fde6b18bd-tuctd00aba3&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMTI3OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwiiNRDiRhgyZGMI1xYQ1R8YI2RjCJ5BEJVYGDZkYwjSAxDgBhgIZGMIlhQQmBwYGGRjCPQUEJ4dGB9kYwikJxCDNRgvZHgBgAGsTYgBybuz6gGQAReYAar2s9_oMdsBEAHcAQ"
+  },
+  {
+    "type": "video",
+    "thumbnail": [
+      {
+        "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fcdn-elle.ladmedia.fr%2Fvar%2Fplain_site%2Fstorage%2Fimages%2Felle-a-table%2Fles-dossiers-de-la-redaction%2Fnews-de-la-redaction%2Fcomment-ne-pas-rater-ses-dim-sum-2696331%2F46939294-1-fre-FR%2FComment-ne-pas-rater-ses-dim-sum_reference.jpg"
+      }
+    ],
+    "description": "Les dim sum sont des bouchées d’origine chinoise, réalisés à partir de pâte fourrée à la viande, aux crevettes, aux légumes, ou parfois même d’une farce sucrée. Ils sont traditionnellement cuits à la vapeur dans des paniers de bambou, même si certains sont frits à la poêle pour un...",
+    "name": "Comment ne pas râter ses dim sum ? - Elle à Table",
+    "created": "Thu, 25 Jan 2024 00:00:00 UTC",
+    "branding": "Cuisine",
+    "duration": "0",
+    "views": "0",
+    "categories": [
+      "en"
+    ],
+    "id": "~~V1~~6213835737286534621~~5khWG3r4qdI2H7O_KccoPngEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePej_D75KE7fubJO0yBBV-Kjn3NvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ16geeKmBzsC8tOXDDFySaFY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe",
+    "origin": "sponsored",
+    "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__98990284f486efafa2557dea6d2b2e2b__7a48870ae0318c93bf89a2be46b1e6ec&response.session=v2_a6a5ab31ae87750c87c46faf01c95b38_bb1dd47d-60fc-4770-8aae-558fde6b18bd-tuctd00aba3_1711744547_1711744547_CNawjgYQswsYqvaz3-gxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E6213835737286534621%7E%7E5khWG3r4qdI2H7O_KccoPngEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePej_D75KE7fubJO0yBBV-Kjn3NvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ16geeKmBzsC8tOXDDFySaFY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe&item.type=video&sig=975a85c20201cc940a937d95a843e2d28862a7216d90&redir=http%3A%2F%2Fwww.elle.fr%2FElle-a-Table%2FLes-dossiers-de-la-redaction%2FNews-de-la-redaction%2FComment-ne-pas-rater-ses-dim-sum-2696331%3Futm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiD9r8gQ15Vcstvn4Lp2FpJ_7JqJra4_4SIYpogG_z_zYCC5BCiIodOMkKfBnbsB%23tblciGiD9r8gQ15Vcstvn4Lp2FpJ_7JqJra4_4SIYpogG_z_zYCC5BCiIodOMkKfBnbsB&ui=bb1dd47d-60fc-4770-8aae-558fde6b18bd-tuctd00aba3&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMTI3OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwiiNRDiRhgyZGMI1xYQ1R8YI2RjCJ5BEJVYGDZkYwjSAxDgBhgIZGMIlhQQmBwYGGRjCPQUEJ4dGB9kYwikJxCDNRgvZHgBgAGsTYgBybuz6gGQAReYAar2s9_oMdsBEAHcAQ"
+  },
+  {
+    "type": "video",
+    "thumbnail": [
+      {
+        "url": "https://images.taboola.com/taboola/image/fetch/f_jpg%2Cq_auto%2Cc_fill%2Cg_faces:auto%2Ce_sharpen/http%3A%2F%2Fcdn-elle.ladmedia.fr%2Fvar%2Fplain_site%2Fstorage%2Fimages%2Felle-a-table%2Fles-dossiers-de-la-redaction%2Fnews-de-la-redaction%2Fon-a-repere-les-crayons-condiment-2684841%2F46614136-1-fre-FR%2FOn-a-repere-les-crayons-condiment_reference.jpg"
+      }
+    ],
+    "description": "Pour dérider un dîner trop guindé, ces crayons exquis devraient faire l’affaire. A chacun son goût : encre de seiche pour parfumer un risotto, piment pour corser une entrecôte, sauce soja pour accompagner des sushis, et vinaigre pour relever des crudités. Le secret de ces nouveaux crayons de...",
+    "name": "On a repéré les crayons-condiment - Elle à Table",
+    "created": "Wed, 05 Mar 2014 00:00:00 UTC",
+    "branding": "Cuisine",
+    "duration": "0",
+    "views": "0",
+    "categories": [
+      "en"
+    ],
+    "id": "~~V1~~-733434860147099299~~5khWG3r4qdI2H7O_KccoPngEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePej-11aCopvuNZexgMTEG6oN3NvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ16geeKmBzsC8tOXDDFySaFY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe",
+    "origin": "sponsored",
+    "url": "https://api.taboola.com/1.0/json/taboola-templates/recommendations.notify-click?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&response.id=__98990284f486efafa2557dea6d2b2e2b__7a48870ae0318c93bf89a2be46b1e6ec&response.session=v2_a6a5ab31ae87750c87c46faf01c95b38_bb1dd47d-60fc-4770-8aae-558fde6b18bd-tuctd00aba3_1711744547_1711744547_CNawjgYQswsYqvaz3-gxIAEoATBmOIjrCkCvkBBI9dLYA1D___________8BWABgAGjGx7Pdg7-4oF5wAQ&item.id=%7E%7EV1%7E%7E-733434860147099299%7E%7E5khWG3r4qdI2H7O_KccoPngEqsl8ynEPWU6g_gQg9GWKANNcaafC-0QP27WPePej-11aCopvuNZexgMTEG6oN3NvVRSOQU3KG74h40zV1xE3Jpvz87tKk9szttPlHlQ16geeKmBzsC8tOXDDFySaFY8XwLbI--WZuj80gUuMjckv-g3BPZqv3IrMOsHf8vWe&item.type=video&sig=6b5d8237f15e305dbd368d77f2bb6a0716f8b7ec6869&redir=http%3A%2F%2Fwww.elle.fr%2FElle-a-Table%2FLes-dossiers-de-la-redaction%2FNews-de-la-redaction%2FOn-a-repere-les-crayons-condiment-2684841%3Futm_source%3Dtaboola%26utm_medium%3Dreferral%26tblci%3DGiD9r8gQ15Vcstvn4Lp2FpJ_7JqJra4_4SIYpogG_z_zYCC5BCjJ6Zic_dOmwxI%23tblciGiD9r8gQ15Vcstvn4Lp2FpJ_7JqJra4_4SIYpogG_z_zYCC5BCjJ6Zic_dOmwxI&ui=bb1dd47d-60fc-4770-8aae-558fde6b18bd-tuctd00aba3&cpb=GIkFIJz__________wEqFnRhYm9vbGFzeW5kaWNhdGlvbi5jb20yCHRyYzAwMTI3OICElKcFQIjrCkivkBBQ9dLYA1j___________8BYwiiNRDiRhgyZGMI1xYQ1R8YI2RjCJ5BEJVYGDZkYwjSAxDgBhgIZGMIlhQQmBwYGGRjCPQUEJ4dGB9kYwikJxCDNRgvZHgBgAGsTYgBybuz6gGQAReYAar2s9_oMdsBEAHcAQ"
+  }
+];
+var sponsoredRecommendations = null;
+var sponsoredsFiltered = [];
+
 function getSponsoredRecommendations() {
   var url = "http://api.taboola.com/1.0/json/taboola-templates/recommendations.get?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&count=100&source.type=video&source.id="
 
   httpRequest(url, function (response) {
-    store.dispatch('setSponsoredRecommendations', [response.list]);
     console.log('httpRequest', response);
+    sponsoreds = response.list;
+    generateBanner();
   });
-
 }
 
-function httpRequest(url, callback, options) {
-
-  var options = {
-    headers: {},
-    body: {}
-  };
+function httpRequest(url, callback, options = { headers: {}, body: {} }) {
 
   var xhr = new XMLHttpRequest();
 
@@ -46,62 +128,55 @@ function httpRequest(url, callback, options) {
 
 }
 
-function State() {
-  this.subscriptions = {};
-}
-
-State.prototype.subscribe = function (element, action, callback) {
-  this.subscriptions[action] = this.subscriptions[action] || [];
-  this.subscriptions[action].push(function (data) {
-    callback.apply(element, data);
-  });
-};
-
-State.prototype.dispatch = function (action, data) {
-  data = data || [];
-
-  if (typeof this[action] === "function") {
-    this[action].apply(this, data);
-  }
-
-  data.push(action);
-  data.push(this);
-
-  this.subscriptions[action] = this.subscriptions[action] || [];
-  var subscriptions = this.subscriptions[action];
-  for (var i = 0; i < subscriptions.length; i++) {
-    subscriptions[i](data);
-  }
-};
-
-var store = new State();
-store.sponsoredRecommendations = [];
-
-store.setSponsoredRecommendations = function (data) {
-  this.sponsoredRecommendations.push(data);
-};
-
-store.removeItemSponsoredRecommendations = function (id, category) {
-  var self = this;
-  this.sponsoredRecommendations[0][category] = this.sponsoredRecommendations[0][category].filter(function (item) {
-    return item.id !== id;
-  });
-};
-
-var sponsoreds = document.getElementsByClassName('rwt');
-
-var sponsoredsFiltered = [];
-for (var i = 0; i < sponsoreds.length; i++) {
-  var element = sponsoreds[i];
-  if (element.getAttribute('type') === 'sponsored') {
-    sponsoredsFiltered.push(element);
+function generateBanner() {
+  for (const element of sponsoredsFiltered) {
+    var component = bannerImgComponent(data[0]);
+    element.appendChild(component);
   }
 }
 
-store.subscribe(sponsoredsFiltered, "setSponsoredRecommendations", function (recommendations, action, store) {
-  console.log(recommendations);
-});
+function bannerImgComponent(data) {
+  var container = document.createElement('div');
+  var template = document.createElement("template");
+
+  template.innerHTML =
+    '<div class="root">' +
+    '   <a target="_blank" class="link">' +
+    '       <img class="img"/>' +
+    '       <h1 class="title"></h1>' +
+    '   <a>' +
+    '   <div class="type">' +
+    '       <span class="branding"></span> | ' +
+    '       <span class="origin"></span>' +
+    '   </div>' +
+    '</div>';
+
+  var root = template.content.getElementsByClassName('root')[0];
+  var link = template.content.getElementsByClassName('link')[0];
+  var img = template.content.getElementsByClassName('img')[0];
+  var title = template.content.getElementsByClassName('title')[0];
+  var origin = template.content.getElementsByClassName('origin')[0];
+  var branding = template.content.getElementsByClassName('branding')[0];
+
+
+  title.textContent = data.name;
+  img.setAttribute('src', data.thumbnail[0].url);
+  link.setAttribute('href', data.url);
+  origin.textContent = data.origin;
+  branding.textContent = data.branding;
+
+  container.appendChild(template.content);
+
+  return container;
+}
 
 document.addEventListener("DOMContentLoaded", function () {
+  sponsoreds = document.getElementsByClassName('rwt');
+  for (var i = 0; i < sponsoreds.length; i++) {
+    var element = sponsoreds[i];
+    if (element.getAttribute('type') === 'sponsored') {
+      sponsoredsFiltered.push(element);
+    }
+  }
   getSponsoredRecommendations();
 });
